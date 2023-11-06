8fab73633b5dd844c0c9ec6b16d48a35be6ac441
Kandapriya <150006391+Kandapriya@users.noreply.github.com>
2023-11-06 12:37:25 +0400

    Add files via upload

---
+++ day1/Firefox.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Float BrowserVersion = 100.2f;
+		String BrowserName = "Firefox";
+		Boolean isVisible = true;
+		int ReleaseYear = 1998;
+		char BrowserLogo = 'F';
+		
+		System.out.println("BrowserName:"+BrowserName + "\nBrowser Version:" + BrowserVersion + "\nReleaseYear" + ReleaseYear+"\nBrowser logo:"+BrowserLogo+"\nVisible"+isVisible);
+		
+				
+		
+		
+				
+
+	}
+
+}
---
+++ day1/Switch.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class Switch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String browser="chrome";
+		switch(browser)
+		{
+			case "Edge":
+				System.out.println("Edge Browser");
+			break;
+			case "Firefox":
+				System.out.println("firefox Browser");
+				break;
+			case " chrome":
+				System.out.println("chrome browser");
+				
+				default: 
+					System.out.println("no browser");
+			
+		}
+		
+		
+	}
+
+}
